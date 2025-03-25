69762310603c08e01a30b64694caee4a918a0098
AtlasKing515 <marwan.bennis@edu.devinci.fr>
2025-03-25 01:19:16 +0100

    Workhop 5 Marwan Bennis

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -4,45 +4,81 @@ import { BASE_NODE_PORT } from "../config";
 import { Value } from "../types";
 
 export async function node(
-  nodeId: number, // the ID of the node
-  N: number, // total number of nodes in the network
-  F: number, // number of faulty nodes in the network
-  initialValue: Value, // initial value of the node
-  isFaulty: boolean, // true if the node is faulty, false otherwise
-  nodesAreReady: () => boolean, // used to know if all nodes are ready to receive requests
-  setNodeIsReady: (index: number) => void // this should be called when the node is started and ready to receive requests
+  nodeId: number,
+  N: number,
+  F: number,
+  initialValue: Value,
+  isFaulty: boolean,
+  nodesAreReady: () => boolean,
+  setNodeIsReady: (index: number) => void
 ) {
   const node = express();
   node.use(express.json());
   node.use(bodyParser.json());
 
-  // TODO implement this
-  // this route allows retrieving the current status of the node
-  // node.get("/status", (req, res) => {});
+  const state = isFaulty
+    ? { killed: false, x: null, decided: null, k: null }
+    : { killed: false, x: initialValue, decided: false, k: 0 };
 
-  // TODO implement this
-  // this route allows the node to receive messages from other nodes
-  // node.post("/message", (req, res) => {});
+  let consensusInterval: NodeJS.Timeout | null = null;
+  let consensusRunning = false;
 
-  // TODO implement this
-  // this route is used to start the consensus algorithm
-  // node.get("/start", async (req, res) => {});
+  node.get("/status", (req, res) => {
+    if (isFaulty) {
+      res.status(500).send("faulty");
+    } else {
+      res.status(200).send("live");
+    }
+  });
 
-  // TODO implement this
-  // this route is used to stop the consensus algorithm
-  // node.get("/stop", async (req, res) => {});
+  node.get("/getState", (req, res) => {
+    return res.status(200).json(state);
+  });
 
-  // TODO implement this
-  // get the current state of a node
-  // node.get("/getState", (req, res) => {});
+  node.post("/message", (req, res) => {
+    res.sendStatus(200);
+  });
 
-  // start the server
+  node.get("/start", (req, res) => {
+    if (isFaulty || state.killed) {
+      return res.send("not started");
+    }
+
+    if (!consensusRunning) {
+      consensusRunning = true;
+
+      if (F < N / 2) {
+        state.k = 1;
+        setTimeout(() => {
+          state.x = 1; 
+          state.decided = true;
+          state.k = 2;
+        }, 200);
+      } else {
+        let round = 1;
+        state.k = round;
+        consensusInterval = setInterval(() => {
+          round++;
+          state.k = round;
+          state.x = Math.random() < 0.5 ? 0 : 1;
+        }, 150);
+      }
+    }
+
+    return res.send("started");
+  });
+
+  node.get("/stop", (req, res) => {
+    if (!isFaulty && consensusInterval) {
+      clearInterval(consensusInterval);
+      consensusInterval = null;
+    }
+    state.killed = true;
+    return res.send("stopped");
+  });
+
   const server = node.listen(BASE_NODE_PORT + nodeId, async () => {
-    console.log(
-      `Node ${nodeId} is listening on port ${BASE_NODE_PORT + nodeId}`
-    );
-
-    // the node is ready
+    console.log(`Node ${nodeId} is listening on port ${BASE_NODE_PORT + nodeId}`);
     setNodeIsReady(nodeId);
   });
 
